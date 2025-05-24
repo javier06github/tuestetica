1e82a5d8aafeb15990e3ea800f2cc1d5013581b5
Javier git <licrissojavier@gmail.com>
2025-05-24 19:30:02 -0300

    2commit

--- src/components/Ale.jsx
+++ src/components/Ale.jsx
@@ -1,86 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "../styles/Productos.css";
+import { dispararSweetBasico } from "../assets/SweetAlert";
+import Card from "./Card"
+import Carrito from "./Carrito"
 
-function Contacto() {
-  const [formulario, setFormulario] = useState({
-    nombre: "",
-    email: "",
-    mensaje: "",
-  });
+function ProductosContainer1({functionCarrito} ){
+    const { id } = useParams();
+    const [productos, setProductos] = useState([])
+    const [cantidad, setCantidad] = useState(1);
+    //const [productosCarrito, setProductosCarrito] = useState([])
+    const [cargando, setCargando] = useState(true);
+    const [error, setError] = useState(null);
+    //const [total, setTotal] = useState(0)
 
-  const handleChange = (e) => {
-    setFormulario({ ...formulario, [e.target.name]: e.target.value });
-  };
+    {useEffect(() =>{
+        fetch('https://68100dc827f2fdac24102255.mockapi.io/productos')
+        .then((res) => res.json())
+            .then((datos) => {
+                console.log(datos)
+                setProductos(datos)
+                setCargando(false);
+            })
+            .catch((error) => {
+                console.log("Error", error)
+                setError('Hubo un error al cargar los productos.');
+                setCargando(false);
+            });
+    }, []);}
 
-  return (
-    <section style={{
-      padding: "3rem 1rem",
-      maxWidth: "600px",
-      margin: "auto",
-      backgroundColor: "#fff0f5",
-      borderRadius: "12px",
-      boxShadow: "0 4px 12px rgba(0,0,0,0.05)"
-    }}>
-      <h2 style={{ color: "#bf1650", fontSize: "2rem", textAlign: "center", marginBottom: "1rem" }}>
-        Contactanos
-      </h2>
-      <form style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-        <input
-          type="text"
-          name="nombre"
-          placeholder="Tu nombre"
-          value={formulario.nombre}
-          onChange={handleChange}
-          style={{
-            padding: "0.75rem",
-            borderRadius: "8px",
-            border: "1px solid #ccc",
-            fontSize: "1rem"
-          }}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Tu email"
-          value={formulario.email}
-          onChange={handleChange}
-          style={{
-            padding: "0.75rem",
-            borderRadius: "8px",
-            border: "1px solid #ccc",
-            fontSize: "1rem"
-          }}
-        />
-        <textarea
-          name="mensaje"
-          placeholder="Tu mensaje"
-          value={formulario.mensaje}
-          onChange={handleChange}
-          rows="5"
-          style={{
-            padding: "0.75rem",
-            borderRadius: "8px",
-            border: "1px solid #ccc",
-            fontSize: "1rem",
-            resize: "none"
-          }}
-        />
-        <button
-          type="button"
-          style={{
-            padding: "0.75rem",
-            backgroundColor: "#bf1650",
-            color: "white",
-            border: "none",
-            borderRadius: "8px",
-            fontSize: "1rem",
-            cursor: "pointer"
-          }}
-        >
-          Enviar mensaje
-        </button>
-      </form>
-    </section>
-  );
+    if (cargando) {
+        return <p>Cargando productos...</p>;
+    }else if (error){
+        return <p>{error}</p>;
+    }else{
+
+        return(
+
+            <div className="productos-container">
+                 {productos.map((producto) => (
+                     <Card
+                        producto={producto}
+                    />
+                 ))}
+            </div>
+
+    // <div>
+//     <Carrito
+//         productosCarrito={productosCarrito}
+//         total={total}
+//     />
+// </div>
+
+
+        )
+    }
 }
 
-export default Contacto;
+export default ProductosContainer1
